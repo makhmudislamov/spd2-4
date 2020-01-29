a762993915c8b936f43658f3c86b5e1e052605d3
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-01-28 19:45:47 -0800

    pseudocode

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,19 @@
 def find_sum(arr, t):
-    
+    # sort the array in ascending order
+    # keep one pointer at the beginning of the arr
+    # another one at the end
+    # check the sum of the both
+    # return new array if its equal to target
+    # if sum is greater than targer
+    # move the right pointer to left until its equal or less then sum
+    # if sum is less than sum
+    # move the left pointer to right
+    # return list(left and right nums) when its equal to target
+
+
+
     pass
+
+
+
+
