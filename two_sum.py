445deb106cc7e17886e66803bbfb7e1351656425
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-01-29 15:35:54 -0800

    case - sum is equal to target

--- two_sum.py
+++ two_sum.py
@@ -1,4 +1,4 @@
-def find_sum(arr, t):
+def find_sum(arr, target):
     # sort the array in ascending order
     # keep one pointer at the beginning of the arr
     # another one at the end
@@ -12,13 +12,18 @@ def find_sum(arr, t):
 
     sorted_arr = sorted(arr)
 
-    print(sorted_arr)
+    left_pointer = 0
+    right_pointer = len(sorted_arr)-1
 
-    pass
-
+    while left_pointer < len(sorted_arr):
+        while right_pointer > 0:
+            pointer_sum = sorted_arr[left_pointer] + sorted_arr[right_pointer]
+            if pointer_sum = target:
+                return list(sorted_arr[left_pointer], sorted_arr[right_pointer])
+            pass
 
 
 
 arr = [5, 3, 6, 8, 2, 4, 7]
-t = 10
+target = 10
 print(find_sum(arr, t))
