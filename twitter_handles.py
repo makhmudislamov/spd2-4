b8bd2b7f356592c3ed420085434f9c20b09c9237
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-01-29 19:18:33 -0800

    v1 - pseudocode

--- twitter_handles.py
+++ twitter_handles.py
@@ -3,5 +3,15 @@ reuturn the highest score handles based on common chars.
 """
 
 def similarity_score(new_handle, all_handles):
+    # should compare two strings and
+    # +1 for common char
+    # -1 for non-common char
+    # return the total score
+
     pass
 
+
+
+
+new_handle = 'iLoveDogs'
+all_handles = 'DogeCoin'
