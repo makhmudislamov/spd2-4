824c3c571a0c9276bd457d4311a4b0f07e302a53
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-01-29 19:32:40 -0800

    v1 code

--- twitter_handles.py
+++ twitter_handles.py
@@ -8,10 +8,23 @@ def similarity_score(new_handle, all_handles):
     # -1 for non-common char
     # return the total score
 
-    pass
+    score = 0
+    new_handle = set(new_handle)
+    all_handles = set(all_handles)
+    print(all_handles)
+    for char in new_handle:
+        
+        if char in all_handles:
+            score += 1
+        else:
+            score -= 1
+        print(char, score)
+    return score
 
 
 
 
 new_handle = 'iLoveDogs'
-all_handles = 'DogeCoin'
+all_handles = 'eDogeCoin'
+
+print(similarity_score(new_handle, all_handles))
