79da8142e0c71803cc5f164130de227529bc7a53
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-01-29 19:17:00 -0800

    started new problem

---
+++ twitter_handles.py
@@ -0,0 +1,7 @@
+"""
+reuturn the highest score handles based on common chars.
+"""
+
+def similarity_score(new_handle, all_handles):
+    pass
+
--- two_sum.py
+++ two_sum.py
@@ -18,7 +18,7 @@ def find_sum(arr, target):
     while left_pointer < len(sorted_arr):
         while right_pointer > 0:
             pointer_sum = sorted_arr[left_pointer] + sorted_arr[right_pointer]
-            if pointer_sum = target:
+            if pointer_sum == target:
                 return list(sorted_arr[left_pointer], sorted_arr[right_pointer])
             pass
 
@@ -26,4 +26,4 @@ def find_sum(arr, target):
 
 arr = [5, 3, 6, 8, 2, 4, 7]
 target = 10
-print(find_sum(arr, t))
+print(find_sum(arr, target))
