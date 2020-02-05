b13e786d25fb422592e30cfb0dca9d31424d8b6f
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-02-04 18:31:25 -0800

    bugfix: pair not found

--- two_sum.py
+++ two_sum.py
@@ -19,24 +19,24 @@ def find_sum(arr, target):
 
     left_pointer = 0
     right_pointer = len(sorted_arr)-1
-    found = []
+    found_pair = []
 
     while left_pointer < len(sorted_arr):
         while right_pointer > 0:
             pointers_sum = sorted_arr[left_pointer] + sorted_arr[right_pointer]
+            if left_pointer == right_pointer:
+                return []
             if pointers_sum == target:
                 #  print(sorted_arr[left_pointer], sorted_arr[left_pointer])
-                found.append(sorted_arr[left_pointer])
-                found.append(sorted_arr[right_pointer])
-                return found
+                found_pair.append(sorted_arr[left_pointer])
+                found_pair.append(sorted_arr[right_pointer])
+                return found_pair
             elif pointers_sum > target:
                 right_pointer -= 1
             elif pointers_sum < target:
                 left_pointer += 1
         
 
-
-
-arr = [5, 6, 6, 8, 1, 4, 7]
-target = 10
+arr = [5, 6, 6, 8,  2, 4, 7]
+target = 16
 print(find_sum(arr, target))
