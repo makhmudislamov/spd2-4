32ddd9327c571ec82fead9ff21c743daee979ff7
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-02-11 19:09:01 -0800

    started these problems

---
+++ ll_problems.py
@@ -0,0 +1,13 @@
+"""
+Linked List Problems:
+* Given a singly-linked list, find the middle item in the list.
+* Given a singly-linked list, reverse the order of the list by modifying the nodes’ links.
+* Given a linked list, rearrange the elements by interleaving the first half of the linked list with the second half.
+Example: If the given linked list is A → B → C → D → E → F → G → H, nodes should be rearranged so the list becomes A → C → E → G → B → D → F → H.
+* Rotate a given linked list counter-clockwise by k nodes, where k is a given integer.
+Example: If the given linked list is A → B → C → D → E → F and k is 4, nodes should be modified so the list becomes E → F → A → B → C → D.
+Assumptions: k is positive and smaller than n, the length of the linked list.
+* Given an array of k linked lists, each of whose items are in sorted order, 
+combine all nodes (do not create new nodes) into a single linked list with all items in order.
+
+"""
