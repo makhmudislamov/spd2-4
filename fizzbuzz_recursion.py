946b5cb222e61d0ab2bff03d642463413e0c25d1
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-02-19 18:26:23 -0800

    pseudocode

--- fizzbuzz_recursion.py
+++ fizzbuzz_recursion.py
@@ -1,8 +1,27 @@
 """
 Let’s practice recursion on a very well-known (and pretty tired, tbh) interview problem.
-You’re asked to write recursive FizzBuzz. Your function takes 2 integers: start and end, 
-which are the first and last number in the sequence of integers to return in a list (array). However, if the number is a multiple of 3, put “Fizz” in the list instead of the number. If it’s a multiple of 5 put “Buzz” in the list. If it’s a multiple of 3 and 5, put “FizzBuzz” in the list.
+You’re asked to write recursive FizzBuzz. 
+Your function takes 2 integers: start and end, 
+which are the first and last number in the sequence of integers to return in a list (array). 
+However, if the number is a multiple of 3, put “Fizz” in the list instead of the number. 
+If it’s a multiple of 5 put “Buzz” in the list. If it’s a multiple of 3 and 5, put “FizzBuzz” in 
+the list.
 Example: fizzbuzz(1, 20) ⇒ [1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, 
 Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz]
 """
 
+def recursive_fizzy(start, end):
+    # declare start
+    # declare empty array
+    # if start % 3 == 0:
+    # append "Fizz"
+    # elif start % 5 == 0:
+    # appemd "Buzz"
+    # elif start % 3 and start % 5:
+    # append "FizzBuzz"
+    # else append the number itself
+    # return the array
+
+    # TASK1
+    # recursively add numbers to array
+    pass
