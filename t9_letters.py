15423d4b7b5c2b72c92f154eed89e5c734fd9fae
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-02-26 19:20:04 -0800

    first attempt - has bugs

--- t9_letters.py
+++ t9_letters.py
@@ -6,5 +6,30 @@ Example execution 1:  t9_letters("23")  ⇒  ["ad", "ae", "af", "bd", "be", "bf"
 Example execution 2:  t9_letters("4663")  ⇒  ["gmmd", …, "gone", …, "good", …, "home", …, "hood", …, "ioof"]
 """
 
-def t9_combos(digits):
-    pass
+T9_LETTERS = {  "2" : "abc", 
+                "3" : "def",
+                "4" : "ghi",
+                "5" : "jkl",
+                "6" : "mno",
+                "7" : "pqrs",
+                "8" : "tuv",
+                "9" : "wxyz"               
+                }
+
+def t9_combos(digits, perms = []):
+    if len(digits) == 0:
+        return perms
+    letters = T9_LETTERS[digits[0]]
+    print(letters)
+    perms.append(letters)
+    # print(perms)
+    new_perms = []
+    for perm in perms:
+        print("perm", perm)
+        for letter in letters:
+            # print("letter", letter)
+            new_perms.append(perm + letter)
+    return t9_combos(digits[1:], new_perms)
+    
+
+print(t9_combos("23", perms=[]))
